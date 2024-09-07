9e31346e1858350b46b636c327d8fde8ac3db938
Diogo Neto <168118296+DiogoNet0@users.noreply.github.com>
2024-09-07 16:20:36 +0100

    changed lsp, treesitter and added a markdown plugin

---
+++ nvim/.config/nvim/lua/shibby/plugins/markdown.lua
@@ -0,0 +1,10 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {},
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "echasnovski/mini.nvim",
+    "echasnovski/mini.icons",
+    "nvim-tree/nvim-web-devicons",
+  }, -- if you use standalone mini plugins
+}
