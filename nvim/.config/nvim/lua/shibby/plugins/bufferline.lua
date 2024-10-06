490e775a99edc4225696ea17da178b48b5394f8d
Diogo Neto <168118296+DiogoNet0@users.noreply.github.com>
2024-10-06 11:52:12 +0100

    removed filename plugin and modified noice.lua neovim

--- nvim/.config/nvim/lua/shibby/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/shibby/plugins/bufferline.lua
@@ -1,16 +1,15 @@
 return {
-  "akinsho/bufferline.nvim",
+  "folke/noice.nvim",
   event = "VeryLazy",
-  keys = {
-    { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-    { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-  },
   opts = {
-    options = {
-      mode = "tabs",
-      -- separator_style = "slant",
-      show_buffer_close_icons = false,
-      show_close_icon = false,
-    },
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
   },
 }
